f086f7533ad211ea3c0fd5dbb4254cde733dbfda
Aldie Alejandro <142805904+TheAAlejandro@users.noreply.github.com>
2025-11-30 23:11:33 +0800

    So many changes

--- elections/settings.py
+++ elections/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'province.apps.ProvinceConfig'
+    'province.apps.ProvinceConfig',
 ]
 
 MIDDLEWARE = [
--- province/models.py
+++ province/models.py
@@ -1,4 +1,3 @@
 from django.db import models
 
 # Create your models here.
-# Note: All models will be connected to politicians/models.py
